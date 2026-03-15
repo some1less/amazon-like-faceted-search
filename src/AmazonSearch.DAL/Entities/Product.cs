23639d20893acb3dbada74cde0dad6d406df41d2
some1less <muracks.max@gmail.com>
2026-03-15 17:52:33 +0100

    Feat: Created base entity

---
+++ src/AmazonSearch.DAL/Entities/Product.cs
@@ -0,0 +1,10 @@
+namespace AmazonSearch.DAL.Entities;
+
+public class Product
+{
+    public string Id { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public string? ImageUrl { get; set; }
+    public string? Brand { get; set; }
+    public List<string> Categories { get; set; } = [];
+}
